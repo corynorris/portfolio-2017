53b30922800f536a7c23506071ef4e38c51ed0b7
corynorris <corynorris@gmail.com>
2016-01-14 19:24:53 -0500

    Attempt #2 to fix viewport

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,14 +28,13 @@
 <section id="introduction">
   <div class="row text-center">
     <div class="col-sm-6 col-sm-offset-3 bordered">
+
       <h1>Welcome</h1>
       <p> 
         I'm a Canadian developer from Ontario, Canada. I've been programming for as long as I can remember, starting with my trusty <a href="https://www.google.ca/search?q=tandy+2000&espv=2&biw=799&bih=838&source=lnms&tbm=isch&sa=X&ved=0ahUKEwj4rbP7sqjKAhVD0h4KHWVdDL8Q_AUIBigB">Tandy2000</a>. Since then, I've been on a <a href="/resume">continual pursuit of learning</a> and improving my programming and design ability.
       </p>
       <hr>                    
       <p>For me, design should be <u>3 things</u>:</p>                    
-    </div>
-  </div>
 
   <div class="row text-center">  
     <div class="col-sm-4 col-xs-12">
@@ -48,8 +47,7 @@
       <strong class="red">Complete</strong><br>with information.
     </div>
   </div>
-  <div class="row text-center">
-    <div class="col-sm-6 col-sm-offset-3 bordered">  
+
       <hr>
       <p>
         When it comes to design, I try and follow the 3 click rule, meaning that from any point on a website, a user should be able to accomplish whatever they like in 3 clicks.
@@ -57,7 +55,9 @@
       <p>
        If there's anything you'd like to talk to me about, please feel free to contact me at corynorris@gmail.com. 
      </p>
-   </div>
- </div>
+
+
+     </div>
+  </div>
 </section>
 @endsection
