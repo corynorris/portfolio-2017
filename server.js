0dab5ace90fab04a2cf93938a7a509718b1e0ac1
corynorris <corynorris@gmail.com>
2016-12-15 19:55:13 -0500

    restructured and fixed ejs files

--- server.js
+++ server.js
@@ -1,28 +1,41 @@
 var express = require('express')
-var app = express()
+var mailer = require('express-mailer');
 
-app.set('port', (process.env.PORT || 5000))
+var app = express()
+app.set('port', (process.env.PORT || 8080))
 app.set('view engine', 'ejs');
+
+// css, js, etc
+app.use(express.static('public'))
 
 // welcome page
 app.get('/', function(req, res) {
-    res.render('resources/index');
-});
-
-// about page 
-app.get('/about', function(req, res) {
-    res.render('pages/about');
-});
-
-// contact page 
-app.get('/contact', function(req, res) {
-    res.render('pages/contact');
+    res.render('pages/index', {
+      page: 'homepage'
+    });
 });
 
 // portfolio page 
 app.get('/portfolio', function(req, res) {
-    res.render('pages/portfolio');
+    res.render('pages/portfolio', {
+      page: 'portfolio'
+    });
 });
+
+// resume page 
+app.get('/resume', function(req, res) {
+    res.render('pages/resume', {
+      page: 'resume'
+    });
+});
+
+// contact page 
+app.get('/contact', function(req, res) {
+    res.render('pages/contact', {
+      page: 'contact'
+    });
+});
+
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
