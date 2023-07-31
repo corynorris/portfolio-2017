64d170d925ed1d0e2f7a84f99fdfa42c9341c2c8
cory <corynorris@gmail.com>
2023-07-31 18:06:25 -0400

    disable contact me

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,20 @@
-var pageLoadFunctions = function() {
+var pageLoadFunctions = function () {
 
 
-  // Hide more content indicator after scroll
-    $(window).scroll(function() {
-      if ($(this).scrollTop() >= 50) { //use `this`, not `document`
-          $('#more').css({
-              'visibility': 'hidden'
-          });
-      } else {
-          $('#more').css({
-              'visibility': 'visible'
-          });
-      }
-  });
+    // Hide more content indicator after scroll
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= 50) { //use `this`, not `document`
+            $('#more').css({
+                'visibility': 'hidden'
+            });
+        } else {
+            $('#more').css({
+                'visibility': 'visible'
+            });
+        }
+    });
 
-  $('#more').click(function() {
+    $('#more').click(function () {
         console.log('clicked');
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
@@ -28,11 +28,22 @@ var pageLoadFunctions = function() {
         }
     });
 
+    $(document).ready(function () {
+        console.log('ready')
+        // Override the form submission
+        $('#contact-form').submit(function (event) {
+            // Prevent the default form submission behavior
+            event.preventDefault();
+
+            // Show an alert indicating that the feature is disabled
+            alert('This feature is disabled in the as this page is archived.');
+        });
+    });
 };
 
 pageLoadFunctions();
 
-$(function() {
+$(function () {
     'use strict';
     var $page = $('#smooth'),
         options = {
@@ -42,7 +53,7 @@ $(function() {
             cacheLength: 2,
             onStart: {
                 duration: 750, // Duration of our animation
-                render: function($container) {
+                render: function ($container) {
                     // Add your CSS animation reversing class
                     $container.addClass('is-exiting');
                     // Restart your animation
@@ -51,7 +62,7 @@ $(function() {
             },
             onReady: {
                 duration: 0,
-                render: function($container, $newContent) {
+                render: function ($container, $newContent) {
                     // Remove your CSS animation reversing class
                     $container.removeClass('is-exiting');
                     // Inject the new content
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 let express = require("express");
 let bodyParser = require("body-parser");
-let Mailgun = require("mailgun-js");
+// let Mailgun = require("mailgun-js");
 
 let app = express();
 app.set("port", process.env.PORT || 3000);
@@ -46,41 +46,41 @@ app.get("/contact", (req, res) => {
   });
 });
 
-function emailBody(name, email, message) {
-  return `
-    From: ${name} (${email})
-    ${message}
-  `;
-}
+// function emailBody(name, email, message) {
+//   return `
+//     From: ${name} (${email})
+//     ${message}
+//   `;
+// }
 
 // email
-app.post("/contact", (req, res) => {
-  let mailgun = new Mailgun({
-    apiKey: app.get("api_key"),
-    domain: app.get("domain")
-  });
+// app.post("/contact", (req, res) => {
+//   let mailgun = new Mailgun({
+//     apiKey: app.get("api_key"),
+//     domain: app.get("domain")
+//   });
 
-  let data = {
-    from: app.get("from"),
-    to: "corynorris@gmail.com",
-    subject: "Contact Form Email",
-    text: emailBody(req.body.name, req.body.email, req.body.message)
-  };
+//   let data = {
+//     from: app.get("from"),
+//     to: "corynorris@gmail.com",
+//     subject: "Contact Form Email",
+//     text: emailBody(req.body.name, req.body.email, req.body.message)
+//   };
 
-  mailgun.messages().send(data, (err, body) => {
-    if (err) {
-      res.render("pages/contact", {
-        page: "contact",
-        message: "There was an error sending your email. Please try again."
-      });
-    } else {
-      res.render("pages/contact", {
-        page: "contact",
-        message: "Your email has been sucessfully sent."
-      });
-    }
-  });
-});
+//   mailgun.messages().send(data, (err, body) => {
+//     if (err) {
+//       res.render("pages/contact", {
+//         page: "contact",
+//         message: "There was an error sending your email. Please try again."
+//       });
+//     } else {
+//       res.render("pages/contact", {
+//         page: "contact",
+//         message: "Your email has been sucessfully sent."
+//       });
+//     }
+//   });
+// });
 
 app.listen(app.get("port"), () => {
   console.log("Node app is running at localhost:" + app.get("port"));
